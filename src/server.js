76f8d35a6d12dc3177b687d5668915a8031c9e44
Colm O'Shea <colmoshea@mcqs.com>
2016-07-30 23:00:49 +0100

    Added socket events

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
 "use strict";
 import Server from 'socket.io';
 
-export default function startServer(){
+export function startServer(store){
+    // create new server
     const io = new Server().attach(8090);
+    // subscribe to Redux store
+    store.subscribe(() => io.emit('state', store.getState().toJS()));
+
+    // on connection, emit the current state to the socket
+    io.on('connection', socket => {
+        socket.emit('state', store.getState().toJS());
+        socket.on('action', store.dispatch.bind(store));
+    });
 }
